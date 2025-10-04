10ba17adc20200fa5797a26c412774cfb2f5f57e
mohamed <mohamedcamirr@gmail.com>
2025-10-04 08:48:09 +0300

    create news_app create splash_view create theme_manager create Page_routes create home_view create drawer display api news

--- lib/modules/home/pages/category_details_view.dart
+++ lib/modules/home/pages/category_details_view.dart
@@ -1,17 +1,66 @@
-
 import 'package:flutter/material.dart';
+import 'package:news_app_c15_mon/core/theme/color_pallete.dart';
 import 'package:news_app_c15_mon/modules/home/model/category_data_model.dart';
+import 'package:news_app_c15_mon/modules/home/model/source_data.dart';
+import 'package:news_app_c15_mon/modules/home/pages/articles_list_view.dart';
+import 'package:news_app_c15_mon/modules/home/widgets/tab_bar_item_widget.dart';
+import 'package:news_app_c15_mon/network/api_request.dart';
 
-class CategoryDetailsView extends StatelessWidget {
-  final CategoryDataModel categoryDataModel ;
+class CategoryDetailsView extends StatefulWidget {
+  final CategoryDataModel categoryDataModel;
   const CategoryDetailsView({super.key, required this.categoryDataModel});
 
   @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      DefaultTabController(length: 4, child: TabBar(tabs: []))
-    ],);
-  }
+  State<CategoryDetailsView> createState() => _CategoryDetailsViewState();
 }
 
+class _CategoryDetailsViewState extends State<CategoryDetailsView> {
+  @override
+  Widget build(BuildContext context) {
 
+    return FutureBuilder<List<SourceData>>(
+      future: ApiRequest.getSources(widget.categoryDataModel.id),
+      builder: (
+          BuildContext context,
+          AsyncSnapshot<List<SourceData>> snapshot,
+          ) {
+        if (snapshot.hasError) {
+          return Center(child: Text(snapshot.error.toString()));
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        List<SourceData> sourcesList = snapshot.data ?? [];
+
+        return DefaultTabController(
+          length: sourcesList.length,
+          child: Column(
+            children: [
+              TabBar(
+                isScrollable: true,
+                dividerColor: Colors.transparent,
+                indicatorColor: ColorPallete.generalTextColor,
+                tabs: sourcesList.map((SourceData element) {
+                  return TabBarItemWidget(
+                    sourceData: element,
+                    isSelected: false, //
+                  );
+                }).toList(),
+              ),
+
+              Expanded(
+                child: TabBarView(
+                  children: sourcesList.map((SourceData source) {
+                    return ArticlesListView(sourceData: source);
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
